9c1bfe2b1ff53d554131c3206d90b35c7253b659
Gustavo Vieira <gustavo.grando0909@gmail.com>
2023-08-11 19:03:25 -0300

    V2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//HAMBURGER ICON
 const icons = document.querySelectorAll('.icon');
 icons.forEach (icon => {  
   icon.addEventListener('click', (event) => {
@@ -10,6 +11,18 @@ icons.forEach (icon => {
 const menuToggle = document.querySelector('.navigator')
 const buttonClick = document.querySelector('.nav-icon-5')
 buttonClick.addEventListener('click', ()=>{
-    console.log('funcioandno')
     menuToggle.classList.toggle('openMenu')
 })
+
+// HEADER BACKGROUND CHANGE ON SCROLL
+document.addEventListener('scroll', ()=>{
+  const header = document.querySelector("header")
+  if(window.scrollY > 100){
+    header.classList.add('scrolled')
+    header.classList.remove('unscroll')
+  }else{
+    header.classList.remove('scrolled')
+    header.classList.add('unscroll')
+  }
+})
+
